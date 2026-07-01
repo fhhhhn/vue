0842308c37971a91984255f932128b83f3b3954b
fhhhhn <jamaalsanbergxkaw1809@gamil.com>
2026-07-01 15:53:07 +0800

    修复部分问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../Home.vue'
 import Cart from '../Cart.vue'
 import Detail from '../Detail.vue'
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 加上下面这行配置
-  publicPath: process.env.NODE_ENV === 'production' 
-    ? '/DEMO-1/'   // 把这里换成你即将在 GitHub 上创建的仓库名字
-    : '/'
+  // 加上下面这行配置，注意前后的斜杠不要漏掉
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue/' : '/'
 })
